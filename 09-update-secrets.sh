42e0d0b4ee8bae25725a70cef2866482b3a2f8cb
edquist <edquist@users.noreply.github.com>
2021-03-09 18:03:28 -0600

    Apply suggestions from code review
    Co-authored-by: Brian Lin <brianhlin@gmail.com>

--- 09-update-secrets.sh
+++ 09-update-secrets.sh
@@ -13,15 +13,4 @@ if [[ -d /etc/condor/tokens-orig.d && -n $(echo /etc/condor/tokens-orig.d/*) ]];
     install -o condor -g condor -m 0600 /etc/condor/tokens-orig.d/* /etc/condor/tokens.d/
 fi
 
-if [[ -d /etc/condor/passwords-orig.d && -n $(echo /etc/condor/passwords-orig.d/*) ]]; then
-    install -o root -g root -m 0600 /etc/condor/passwords-orig.d/* /etc/condor/passwords.d/
-fi
-shopt -u nullglob
-
-if [[ -f /etc/condor/gsi-orig.d/hostcert.pem && -f /etc/condor/gsi-orig.d/hostkey.pem ]]; then
-    install -o root -g root -m 0644 /etc/condor/gsi-orig.d/hostcert.pem /etc/grid-security/hostcert.pem
-    install -o root -g root -m 0600 /etc/condor/gsi-orig.d/hostkey.pem /etc/grid-security/hostkey.pem
-fi
-
-
 # vim:et:sw=4:sts=4:ts=8
