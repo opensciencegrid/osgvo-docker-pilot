20ac2dd0f8dd8eea425a07e570dcdd67aec3e71c
David Schultz <davids24@gmail.com>
2021-09-15 15:48:48 -0500

    Make default policy to auto-restart htcondor, with shut down on idle requiring the variable  to be set

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ fail () {
 cvmfsexec_root=/cvmfsexec
 cvmfsexec_tarball=/cvmfsexec.tar.gz
 cvmfsexec_local_config=$cvmfsexec_root/dist/etc/cvmfs/default.local
+htcondor_supervisord_config=/etc/supervisord.d/10-htcondor.conf
 
 if [[ -d /cvmfs/config-osg.opensciencegrid.org ]]; then
     echo "OSG CVMFS already available (perhaps via bind-mount),"
@@ -34,6 +35,18 @@ add_or_replace () {
     local value="$3"
 
     if grep -Eq "^${var}=" "$file"; then
+        sed -i -r -e "s#^${var}=.*#${var}=${value}#" "$file"
+    else
+        echo "${var}=\"${value}\"" >> "$file"
+    fi
+}
+
+add_or_replace_quoted () {
+    local file="$1"
+    local var="$2"
+    local value="$3"
+
+    if grep -Eq "^${var}=" "$file"; then
         sed -i -r -e "s#^${var}=.*#${var}=\"${value}\"#" "$file"
     else
         echo "${var}=\"${value}\"" >> "$file"
@@ -45,12 +58,15 @@ if [[ -e /cvmfsexec/default.local ]]; then
 fi
 
 if [[ -n $CVMFS_HTTP_PROXY ]]; then
-    add_or_replace "$cvmfsexec_local_config" CVMFS_HTTP_PROXY "${CVMFS_HTTP_PROXY}"
+    add_or_replace_quoted "$cvmfsexec_local_config" CVMFS_HTTP_PROXY "${CVMFS_HTTP_PROXY}"
 fi
 
 if [[ -n $CVMFS_QUOTA_LIMIT ]]; then
-    add_or_replace "$cvmfsexec_local_config" CVMFS_QUOTA_LIMIT "${CVMFS_QUOTA_LIMIT}"
+    add_or_replace_quoted "$cvmfsexec_local_config" CVMFS_QUOTA_LIMIT "${CVMFS_QUOTA_LIMIT}"
 fi
 
+if [ "x$SUPERVISORD_RESTART_POLICY" != "x" ]; then
+    add_or_replace "$htcondor_supervisord_config" autorestart "${SUPERVISORD_RESTART_POLICY}"
+fi
 
 exec $cvmfsexec_root/cvmfsexec -N $CVMFSEXEC_REPOS -- "$@"
