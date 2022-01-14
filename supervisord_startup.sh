f89c882684a17322aa7af9f8fff8992e4576c743
Brian Lin <brianhlin@gmail.com>
2022-01-14 11:55:27 -0600

    Allow empty cleanup dirs
    Co-authored-by: Matyas Selmeci <matyas@cs.wisc.edu>

--- supervisord_startup.sh
+++ supervisord_startup.sh
@@ -11,7 +11,9 @@ shopt -u nullglob
 
 # Allow child images to add cleanup customizations
 function source_cleanup {
+    shopt -s nullglob
     for x in /etc/osg/image-cleanup.d/*.sh; do source "$x"; done
+    shopt -u nullglob
 }
 trap source_cleanup EXIT TERM QUIT
 
