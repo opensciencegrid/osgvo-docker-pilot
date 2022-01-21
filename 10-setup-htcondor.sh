37076a6821b0f1b533c34008bf99a1787e4756be
Matyas Selmeci <matyas@cs.wisc.edu>
2022-01-21 12:54:21 -0600

    Fix indent

--- 10-setup-htcondor.sh
+++ 10-setup-htcondor.sh
@@ -160,25 +160,26 @@ touch /pilot/log/{Master,Start,Proc,SharedPort,XferStats,log/Starter}Log /pilot/
 # Configure remote peer if applicable
 if [[ "x$SYSLOG_HOST" != "x" ]]; then
 
-# Set some reasonable defaults for the token registry.
-if [[ "x$REGISTRY_HOST" != "x" ]]; then
-    REGISTRY_HOSTNAME="$REGISTRY_HOST"
-elif [[ "$SYSLOG_HOST" == "syslog.osgdev.chtc.io" ]]; then
-    REGISTRY_HOSTNAME="os-registry.osgdev.chtc.io"
-else
-    REGISTRY_HOSTNAME="os-registry.opensciencegrid.org"
-fi
+    # Set some reasonable defaults for the token registry.
+    if [[ "x$REGISTRY_HOST" != "x" ]]; then
+        REGISTRY_HOSTNAME="$REGISTRY_HOST"
+    elif [[ "$SYSLOG_HOST" == "syslog.osgdev.chtc.io" ]]; then
+        REGISTRY_HOSTNAME="os-registry.osgdev.chtc.io"
+    else
+        REGISTRY_HOSTNAME="os-registry.opensciencegrid.org"
+    fi
 
-# If hostcert generation fails, then we'll just skip the whole syslog thing.
-generate-hostcert "$_CONDOR_SEC_PASSWORD_FILE" "$REGISTRY_HOSTNAME" || SYSLOG_HOST=""
+    # If hostcert generation fails, then we'll just skip the whole syslog thing.
+    generate-hostcert "$_CONDOR_SEC_PASSWORD_FILE" "$REGISTRY_HOSTNAME" || SYSLOG_HOST=""
+
 fi
 
 if [[ "x$SYSLOG_HOST" != "x" ]]; then
 
-for NAME in Condor Glidein Supervisord
-do
+    for NAME in Condor Glidein Supervisord
+    do
 
-cat >> /pilot/rsyslog/conf/forward.conf << EOF
+    cat >> /pilot/rsyslog/conf/forward.conf << EOF
 ruleset(name="forward${NAME}") {
   action(type="omfwd"
     queue.filename="fwdAll"
@@ -195,11 +196,11 @@ ruleset(name="forward${NAME}") {
 }
 EOF
 
-done
+    done
 
 else
 
-cat > /pilot/rsyslog/conf/forward.conf << EOF
+    cat > /pilot/rsyslog/conf/forward.conf << EOF
 ruleset(name="forwardCondor") {}
 ruleset(name="forwardGlidein") {}
 ruleset(name="forwardSupervisord") {}
