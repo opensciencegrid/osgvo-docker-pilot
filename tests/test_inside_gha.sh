ff87950ed03251c7a505b53ee493fddb3f03b712
Brian Lin <brianhlin@gmail.com>
2022-12-17 14:15:28 -0600

    Start CVMFS once per test run

--- tests/test_inside_gha.sh
+++ tests/test_inside_gha.sh
@@ -37,43 +37,6 @@ function start_singularity_backfill {
             docker-daemon:$CONTAINER_IMAGE > $SINGULARITY_OUTPUT 2>&1 &"
 }
 
-function install_cvmfs {
-    if [[ $- = *e* ]]; then
-        olde="-e"
-    else
-        olde="+e"
-    fi
-    set -e
-    apt-get install lsb-release
-    wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
-    wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-contrib-release/cvmfs-contrib-release-latest_all.deb
-    dpkg -i cvmfs-release-latest_all.deb cvmfs-contrib-release-latest_all.deb
-    rm -f *.deb
-    apt-get update
-    apt-get install -y cvmfs-config-osg cvmfs
-    set $olde
-}
-
-function start_cvmfs {
-    if [[ $- = *e* ]]; then
-        olde="-e"
-    else
-        olde="+e"
-    fi
-    set -e
-    systemctl start autofs
-    mkdir -p /etc/auto.master.d/
-    echo "/cvmfs /etc/auto.cvmfs" > /etc/auto.master.d/cvmfs.autofs
-    cat << EOF | tee /etc/cvmfs/default.local
-CVMFS_REPOSITORIES="$( (echo oasis.opensciencegrid.org;echo cms.cern.ch;ls /cvmfs) | sort -u|paste -sd ,)"
-CVMFS_QUOTA_LIMIT=2000
-CVMFS_HTTP_PROXY="DIRECT"
-EOF
-    systemctl restart autofs
-    ls -l /cvmfs/singularity.opensciencegrid.org
-    set $olde
-}
-
 function start_docker_backfill {
     touch $OSP_TOKEN_PATH
     docker $COMMON_DOCKER_ARGS \
@@ -189,8 +152,6 @@ case "$CVMFS_INSTALL" in
                            --security-opt systempaths=unconfined
                            --security-opt no-new-privileges
                            -v "/cvmfs:/cvmfs:shared")
-        install_cvmfs
-        start_cvmfs
         ;;
     cvmfsexec)
         DOCKER_EXTRA_ARGS=(--privileged
