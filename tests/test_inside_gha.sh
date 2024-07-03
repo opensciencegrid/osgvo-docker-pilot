3ba45287d6844e7fc368e15b32a43f00f45b1275
Matyas Selmeci <matyas@cs.wisc.edu>
2024-07-03 17:12:56 -0500

    need more security

--- tests/test_inside_gha.sh
+++ tests/test_inside_gha.sh
@@ -9,7 +9,7 @@ COMMON_DOCKER_ARGS="run --user osg
                         -e GLIDEIN_Site='None'
                         -e GLIDEIN_ResourceName='None'
                         -e GLIDEIN_Start_Extra='True'
-                        -e _CONDOR_ALL_DEBUG='D_CAT,D_SUB_SECOND,D_PID,D_SECURITY'
+                        -e _CONDOR_ALL_DEBUG='D_CAT,D_SUB_SECOND,D_PID,D_SECURITY:2'
                         -e OSG_SQUID_LOCATION='None'"
 
 function usage {
@@ -133,7 +133,7 @@ function test_docker_HAS_SINGULARITY {
     [[ $ret -eq $ABORT_CODE ]] && { debug_docker_backfill; return $ABORT_CODE; }
     startd_addr=$(run_inside_backfill_container condor_who -log $logdir -dae | awk '/^Startd/ {print $6}'); ret=$?
     [[ $ret -eq $ABORT_CODE ]] && { debug_docker_backfill; return $ABORT_CODE; }
-    has_singularity=$(run_inside_backfill_container condor_status -debug:D_SECURITY -direct "$startd_addr" -af HAS_SINGULARITY); ret=$?
+    has_singularity=$(run_inside_backfill_container condor_status -debug:D_SECURITY:2 -direct "$startd_addr" -af HAS_SINGULARITY); ret=$?
     [[ $ret -eq $ABORT_CODE ]] && { debug_docker_backfill; return $ABORT_CODE; }
     if [[ $has_singularity == 'true' ]]; then
         return 0
