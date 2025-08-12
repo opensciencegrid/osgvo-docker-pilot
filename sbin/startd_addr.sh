c96440089324eeb4d0b2f0567b6ee3dd21c6ecc9
Mats Rynge <mats@rynge.net>
2025-08-12 14:49:14 -0700

    More robust by waiting for log files

--- sbin/startd_addr.sh
+++ sbin/startd_addr.sh
@@ -20,7 +20,7 @@ sys.exit(0 if minimum <= version <= maximum else 1)
 
 
 # wait for the master to come up
-master_timeout=60
+master_timeout=120
 SECONDS=0
 while [ ! -s "$CONDOR_LOGDIR/MasterLog" ]; do
     if [ $SECONDS -gt $master_timeout ]; then
@@ -30,14 +30,25 @@ while [ ! -s "$CONDOR_LOGDIR/MasterLog" ]; do
     sleep 5
 done
 
-# now wait for the startd
+# wait for the startd to log something
+startd_timeout=120
+SECONDS=0
+while [ ! -s "$CONDOR_LOGDIR/StartLog" ]; do
+    if [ $SECONDS -gt $startd_timeout ]; then
+        echo "Timeout: condor_startd did not start within $startd_timeout seconds." >&2
+        exit 1
+    fi
+    sleep 5
+done
+
+# now wait for the startd to be registered
 startd_addr=$(condor_who -log $CONDOR_LOGDIR \
-                         -wait:600 'IsReady && STARTD_State =?= "Ready"' \
+                         -wait:300 'IsReady && STARTD_State =?= "Ready"' \
                          -dae \
-                | awk '/^Startd/ {print $6}')
+                | awk '/^Startd/ {print $4}')
 ret=$?
 
-if [ $ret -ne 0 || -z "$startd_addr" ]; then
+if [[ $ret -ne 0 || -z "$startd_addr" ]]; then
     echo "Unable to determine startd addr" >&2
     exit 1
 fi
